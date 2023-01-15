69c1de04f48bed3ffa2f277bf8865b35f36726d5
pokhrelashok <pokhrelashok2@gmail.com>
2023-01-15 13:11:15 +0545

    Updates

--- _app/immutable/chunks/2-22cc9029.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-b132b374.js";export{t as component};
---
+++ _app/immutable/chunks/2-d482edc7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-54c7a57f.js";export{t as component};
