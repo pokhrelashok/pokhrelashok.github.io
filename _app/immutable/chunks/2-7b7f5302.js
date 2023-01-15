6b0936594341204030a25c84d8f948cd61428e2f
pokhrelashok <pokhrelashok2@gmail.com>
2023-01-15 13:18:37 +0545

    Updates

---
+++ _app/immutable/chunks/2-7b7f5302.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-cad60140.js";export{t as component};
--- _app/immutable/chunks/2-8933c851.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-126263ff.js";export{t as component};
