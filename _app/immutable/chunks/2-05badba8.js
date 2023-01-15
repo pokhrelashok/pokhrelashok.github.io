56096276710ea10c4965e17445f3a5c8fca03599
pokhrelashok <pokhrelashok2@gmail.com>
2023-01-15 13:20:07 +0545

    Updates

---
+++ _app/immutable/chunks/2-05badba8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-204c342f.js";export{t as component};
--- _app/immutable/chunks/2-7b7f5302.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-cad60140.js";export{t as component};
